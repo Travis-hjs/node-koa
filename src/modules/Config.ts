eaaa1c32428de8627782a334b5907a0787042704
黄景圣 <1141752341@qq.com>
2022-07-22 12:26:30 +0800

    代码配置修改

--- src/modules/Config.ts
+++ src/modules/Config.ts
@@ -78,18 +78,6 @@ class ModuleConfig {
    */
   readonly uploadImgName = "img";
 
-  /**
-   * 用户临时表
-   * @description `.gitignore`忽略文件也要添加该路径，因为`jwt`模块中做了动态创建处理，所以不需要代码同步该文件
-  */
-  readonly userFile = "public/user.json";
-
-  /** `token`长度 */
-  readonly tokenSize = 28;
-
-  /** `token`格式错误提示文字 */
-  readonly tokenTip = "无效的token";
-
 }
 
 /** 项目配置 */
